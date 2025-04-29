c678b4d19bf53a78b9e14afab6efb44103e2b0e8
hajajahirhussain <hajajahirhussain@gmail.com>
2025-04-29 09:09:27 +0530

    modified fastapi database.py file

--- fastapi_auth_service/database.py
+++ fastapi_auth_service/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from decouple import config
 
-DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+# DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+DATABASE_URL = f"postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@{config("DB_HOST", default="localhost")}/{config("DB_NAME")}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
