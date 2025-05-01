967238657484ba11b09befbb669e9a858fa2da6a
hajajahirhussain <hajajahirhussain@gmail.com>
2025-05-01 15:23:37 +0530

    added docker

--- fastapi_auth_service/database.py
+++ fastapi_auth_service/database.py
@@ -5,7 +5,7 @@ import os
 from decouple import config
 
 # DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-DATABASE_URL = f"postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@{config("DB_HOST", default="localhost")}/{config("DB_NAME")}"
+DATABASE_URL = DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST', default='host.docker.internal')}/{config('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-((xsk6putcvc9n2i9&2dx0mq%_js=3nz-fhv59tao54(+3kcg3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.11', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -93,7 +95,7 @@ WSGI_APPLICATION = "taskmanager.wsgi.application"
 # del db.sqlite3  # Windows
 
 #Postgres DataBase
-import os
+
 #Using Environmen Variables
 # DATABASES = {
 #     "default": {
@@ -161,7 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
